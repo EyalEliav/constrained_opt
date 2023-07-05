4efc4c626f151264b62974396fbe798f5441131b
EyalEliav <eyaleliav15@gmail.com>
2023-07-05 17:17:47 +0300

    fix function and variables naming according to instructions

--- src/constrained_min.py
+++ src/constrained_min.py
@@ -2,32 +2,40 @@ import numpy as np
 import math
 
 
-def solve_optimization_problem(objective_function, inequality_constraints, equality_constraints_matrix,
-                               equality_constraints_rhs, initial_point):
+def interior_pt(func, ineq_constraints, eq_constraints_mat, eq_constraints_rhs, x0):
     path = []
     values = []
     scale = 1
 
-    current_obj_val, current_gradient, current_hessian = calculate_objective_value(objective_function,
-                                                                                   inequality_constraints,
-                                                                                   initial_point, scale)
+    current_obj_val, current_gradient, current_hessian = calculate_objective_value(
+        func, ineq_constraints, x0, scale
+    )
 
-    num_inequality_constraints = len(inequality_constraints)
-    current_point = initial_point.copy()
+    num_inequality_constraints = len(ineq_constraints)
+    current_point = x0.copy()
 
     path.append(current_point)
-    values.append(objective_function(current_point)[0])
+    values.append(func(current_point)[0])
 
     while num_inequality_constraints / scale > 1e-8:
         for _ in range(10):
-            direction = determine_optimal_direction(current_hessian, equality_constraints_matrix, current_gradient)
-            step_size = perform_backtracking_line_search(objective_function, current_point, current_obj_val,
-                                                         current_gradient, direction, inequality_constraints, scale)
+            direction = determine_optimal_direction(
+                current_hessian, eq_constraints_mat, current_gradient
+            )
+            step_size = perform_backtracking_line_search(
+                func,
+                current_point,
+                current_obj_val,
+                current_gradient,
+                direction,
+                ineq_constraints,
+                scale,
+            )
             next_point = current_point + direction * step_size
 
-            next_obj_val, next_gradient, next_hessian = calculate_objective_value(objective_function,
-                                                                                  inequality_constraints, next_point,
-                                                                                  scale)
+            next_obj_val, next_gradient, next_hessian = calculate_objective_value(
+                func, ineq_constraints, next_point, scale
+            )
 
             lambda_value = np.sqrt(np.dot(direction, np.dot(next_hessian, direction.T)))
             if 0.5 * (lambda_value ** 2) < 1e-8:
@@ -39,15 +47,21 @@ def solve_optimization_problem(objective_function, inequality_constraints, equal
             current_hessian = next_hessian
 
         path.append(current_point.copy())
-        values.append(objective_function(current_point.copy())[0])
+        values.append(func(current_point.copy())[0])
         scale *= 10
 
-    return current_point, objective_function(current_point.copy())[0], {'path': path, 'values': values}
+    return (
+        current_point,
+        func(current_point.copy())[0],
+        {"path": path, "values": values},
+    )
 
 
 def calculate_objective_value(objective_function, inequality_constraints, point, scale):
     obj_val, gradient, hessian = objective_function(point)
-    log_obj_val, log_gradient, log_hessian = apply_logarithmic_barrier(inequality_constraints, point)
+    log_obj_val, log_gradient, log_hessian = apply_logarithmic_barrier(
+        inequality_constraints, point
+    )
     updated_obj_val = scale * obj_val + log_obj_val
     updated_gradient = scale * gradient + log_gradient
     updated_hessian = scale * hessian + log_hessian
@@ -67,21 +81,24 @@ def apply_logarithmic_barrier(inequality_constraints, point):
 
         gradient = g_val / f_val
         gradient_dim = gradient.shape[0]
-        gradient_tile = np.tile(gradient.reshape(gradient_dim, -1), (1, gradient_dim)) * np.tile(
-            gradient.reshape(gradient_dim, -1).T, (gradient_dim, 1))
+        gradient_tile = np.tile(
+            gradient.reshape(gradient_dim, -1), (1, gradient_dim)
+        ) * np.tile(gradient.reshape(gradient_dim, -1).T, (gradient_dim, 1))
         log_hessian += (h_val * f_val - gradient_tile) / f_val ** 2
 
     return -log_obj_val, log_gradient, -log_hessian
 
 
 def determine_optimal_direction_with_equality(hessian, A, gradient):
-    left_matrix = np.block([
-        [hessian, A.T],
-        [A, 0],
-    ])
+    left_matrix = np.block(
+        [
+            [hessian, A.T],
+            [A, 0],
+        ]
+    )
     right_vector = np.block([[-gradient, 0]])
     ans = np.linalg.solve(left_matrix, right_vector.T).T[0]
-    return ans[0:A.shape[1]]
+    return ans[0 : A.shape[1]]
 
 
 def determine_optimal_direction_without_equality(hessian, gradient):
@@ -94,13 +111,26 @@ def determine_optimal_direction(hessian, A, gradient):
     return determine_optimal_direction_without_equality(hessian, gradient)
 
 
-def perform_backtracking_line_search(objective_function, point, obj_val, gradient, direction, inequality_constraints,
-                                     scale, alpha=0.01, beta=0.5, max_iter=10):
+def perform_backtracking_line_search(
+    objective_function,
+    point,
+    obj_val,
+    gradient,
+    direction,
+    inequality_constraints,
+    scale,
+    alpha=0.01,
+    beta=0.5,
+    max_iter=10,
+):
     step_size = 1
     current_obj_val, _, _ = objective_function(point + step_size * direction)
 
     iteration_count = 0
-    while iteration_count < max_iter and current_obj_val > obj_val + alpha * step_size * gradient.dot(direction):
+    while (
+        iteration_count < max_iter
+        and current_obj_val > obj_val + alpha * step_size * gradient.dot(direction)
+    ):
         step_size *= beta
         current_obj_val, _, _ = objective_function(point + step_size * direction)
         iteration_count += 1
